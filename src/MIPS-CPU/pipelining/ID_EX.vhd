9bb2e745fbe616e43b51bda2eb356bff1fbdf228
Mario Morcos Wassily <mariomorcoswassily@gmail.com>
2024-05-20 20:13:53 +0300

    changes

--- src/MIPS-CPU/pipelining/ID_EX.vhd
+++ src/MIPS-CPU/pipelining/ID_EX.vhd
@@ -1,3 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
+
+
+ 
+Entity ID_EX is 
+    port(
+        reg_dst, jump, branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write : in std_logic;
+        pc_plus4 : in std_logic_vector(31 downto 0);
+        read_data1, read_data2 : in std_logic_vector(31 downto 0);
+        extended_sign : in std_logic_vector(31 downto 0);
+        
+    );
