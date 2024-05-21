311be4e694d2af4ca423ab2b49dd400c17022f97
Mario Morcos Wassily <mariomorcoswassily@gmail.com>
2024-05-21 22:04:13 +0300

    fixed EX_MEM

--- src/MIPS-CPU/pipelining/EX_MEM.vhd
+++ src/MIPS-CPU/pipelining/EX_MEM.vhd
@@ -5,13 +5,11 @@ use IEEE.numeric_std.all;
 
 entity EX_MEM is
     port(
-        jump, branch, mem_read, mem_to_reg, mem_write : in std_logic;
-        alu_zero : in std_logic;
+        mem_read, mem_to_reg, mem_write : in std_logic;
         alu_result : in std_logic_vector(31 downto 0);
         read_data2 : in std_logic_vector(31 downto 0);
         clk : in std_logic;
-        jump_out, branch_out, mem_read_out, mem_to_reg_out, mem_write_out : out std_logic;
-        alu_zero_out : out std_logic;
+        mem_read_out, mem_to_reg_out, mem_write_out : out std_logic;
         alu_result_out : out std_logic_vector(31 downto 0);
         read_data2_out : out std_logic_vector(31 downto 0)
     );
@@ -22,13 +20,10 @@ begin
     process(clk)
     begin
         if(rising_edge(clk)) then
-            jump_out <= jump;
-            branch_out <= branch;
             mem_read_out <= mem_read;
             mem_to_reg_out <= mem_to_reg;
             mem_write_out <= mem_write;
 
-            alu_zero_out <= alu_zero;
             alu_result_out <= alu_result;
 
             read_data2_out <= read_data2;
--- src/MIPS-CPU/pipelining/MEM_WB.vhd
+++ src/MIPS-CPU/pipelining/MEM_WB.vhd
@@ -6,5 +6,8 @@ use IEEE.numeric_std.all;
 
 entity MEM_WB is
     port(
+        mem_to_reg, reg_write : in std_logic;
+        read_data : in std_logic_vector(31 downto 0);
+        alu_result : in std_logic_vector(31 downto 0);
         
     );
