f030f973162c067ee9400b939b89c3924c65bb8b
mobyakol3eesh <mohamed_hesham203@icloud.com>
2024-05-22 01:47:24 +0300

    changed reg_des_addr to reg_write_back deleted alu zero

--- src/MIPS-CPU/pipelining/EX_MEM.vhd
+++ src/MIPS-CPU/pipelining/EX_MEM.vhd
@@ -6,11 +6,15 @@ use IEEE.numeric_std.all;
 entity EX_MEM is
     port(
         mem_read, mem_to_reg, mem_write : in std_logic;
+		  reg_write  : in std_logic ;
+		  reg_write_back : in std_logic_vector (4 downto 0);
         alu_result : in std_logic_vector(31 downto 0);
         read_data2 : in std_logic_vector(31 downto 0);
         clk : in std_logic;
         mem_read_out, mem_to_reg_out, mem_write_out : out std_logic;
         alu_result_out : out std_logic_vector(31 downto 0);
+		  reg_write_out  : out std_logic;
+		  reg_write_back_out : out std_logic_vector (4 downto 0);
         read_data2_out : out std_logic_vector(31 downto 0)
     );
 end EX_MEM;
@@ -23,9 +27,9 @@ begin
             mem_read_out <= mem_read;
             mem_to_reg_out <= mem_to_reg;
             mem_write_out <= mem_write;
-
+				reg_write_out <= reg_write;
+				reg_write_back_out <= reg_write_back;
             alu_result_out <= alu_result;
-
             read_data2_out <= read_data2;
         end if;
     end process;
